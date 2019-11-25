9c58f9f2dbd5c83761a08f08e4df8c9cb28dab41
Justin Klass <Justin@Justins-MacBook.local>
2019-11-25 09:42:05 -0500

    Search bar fixed. Added delete and new functionality

--- frontend/src/components/DeleteRental.js
+++ frontend/src/components/DeleteRental.js
@@ -3,29 +3,34 @@ import axios from "axios";
 class DeleteRental extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+        rental: []
+    };
 
     this.deleteRental = this.deleteRental.bind(this);
     this.getRental = this.getRental.bind(this);
   }
+
   async getRental() {
-    const apiData = await axios(`${baseurl}/rental`);
+    const apiData = await axios(`${this.props.baseURL}/rental`);
     const data = apiData.data;
     this.setState({
       rental: data
     });
   }
   async deleteRental(id) {
-    await axios.delete(`${baseUrl}/rental/${id}`);
-    const filteredrentals = this.state.rental.filter(rental => {
-      return rental._id != id;
+    await axios.delete(`${this.props.baseURL}/rental/${id}`);
+    const filteredRentals = this.state.rental.filter(rental => {
+      return rental._id !== id;
     });
     this.setState({
       rental: filteredRentals
     });
   }
   render() {
-    return <button onClick={() => this.deleteRental(rental._id)}></button>;
+      return (
+        <button onClick={() => this.deleteRental(this.props.rental._id)}>Delete</button>
+      )
   }
 }
 export default DeleteRental;
